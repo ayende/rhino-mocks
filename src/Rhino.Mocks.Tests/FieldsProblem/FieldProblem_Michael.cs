65d9a07f31c5b2b30ea13a0697be546c7a127415
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-07-06 23:31:14 +0000

    Added test case.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@81 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ src/Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Michael.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+	[TestFixture]
+	public class FieldProblem_Michael
+	{
+		public interface IProvider
+		{
+			char GetChar();
+			int GetInt32();
+		}
+		
+		[Test]
+		public void TestChar()
+		{
+			MockRepository mocks = new MockRepository();
+			IProvider mockProvider = mocks.CreateMock<IProvider>();
+			SetupResult.For(mockProvider.GetChar()).Return('X');
+			mocks.ReplayAll();
+			Assert.AreEqual('X', mockProvider.GetChar()); // actual is a random char
+		}
+
+		[Test]
+		public void TestInt32()
+		{
+			MockRepository mocks = new MockRepository();
+			IProvider mockProvider = mocks.CreateMock<IProvider>();
+			SetupResult.For(mockProvider.GetInt32()).Return(100);
+			mocks.ReplayAll();
+			Assert.AreEqual(100, mockProvider.GetInt32()); // actual is 100
+		}
+
+	}
+}
