cdc41b8c947c31e5a46aa9ab49a136a0ac20d557
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-04-08 02:52:13 +0000

    Additional test for COM interop mocking.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@533 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Luke.cs
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Luke.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using MbUnit.Framework;
 using mshtml;
 
@@ -13,6 +15,22 @@ namespace Rhino.Mocks.Tests.FieldsProblem
 			IHTMLEventObj2 mock = mockRepository.CreateMock<IHTMLEventObj2>();
 			Assert.IsNotNull(mock);
 		}
+
+		[Test]
+		public void CanMockComInterface()
+		{
+			MockRepository mocks = new MockRepository();
+			IServiceProvider serviceProvider = (IServiceProvider)
+				mocks.CreateMultiMock(typeof(IServiceProvider), typeof (IHTMLDataTransfer));
+			Assert.IsNotNull(serviceProvider);
+		}
 	}
 
+	[ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"),
+	 InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	public interface IServiceProvider
+	{
+		[return : MarshalAs(UnmanagedType.IUnknown)]
+		object QueryService([In] ref Guid guidService, [In] ref Guid riid);
+	}
 }
