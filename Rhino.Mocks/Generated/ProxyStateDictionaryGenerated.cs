0eed15387f5317845cc7b78d77169b5e7de85544
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-04-28 23:45:43 +0000

    Removing 1.1 legacy collections.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@561 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Mocks/Generated/ProxyStateDictionaryGenerated.cs
+++ Rhino.Mocks/Generated/ProxyStateDictionaryGenerated.cs
@@ -1,86 +1,19 @@
-using System.Collections;
+using System.Collections.Generic;
 using Rhino.Mocks.Impl;
 using Rhino.Mocks.Interfaces;
 
 namespace Rhino.Mocks.Generated
 {
-#if dotNet2
-    /// <summary>
-	/// Dictionary class
-	/// </summary>
-    public class ProxyStateDictionary : System.Collections.Generic.Dictionary<object,IMockState>
-    {
-        /// <summary>
-        /// Create a new instance of <c>ProxyStateDictionary</c>
-        /// </summary>
-        public ProxyStateDictionary() : base(MockedObjectsEquality.Instance) { }
-    }
-#else
 	/// <summary>
 	/// Dictionary class
 	/// </summary>
-	public class ProxyStateDictionary
+	public class ProxyStateDictionary : Dictionary<object, IMockState>
 	{
-		Hashtable inner;
-
 		/// <summary>
-		/// Create a new instance
+		/// Create a new instance of <c>ProxyStateDictionary</c>
 		/// </summary>
-		public ProxyStateDictionary()
+		public ProxyStateDictionary() : base(MockedObjectsEquality.Instance)
 		{
-			inner = new Hashtable(MockedObjectsEquality.Instance, MockedObjectsEquality.Instance);	
-		}
-
-		/// <summary>
-		/// Add proxy and state
-		/// </summary>
-		public void Add(object mock, IMockState state)
-		{
-			inner.Add(mock, state);
-		}
-
-		/// <summary>
-		/// Contains a proxy
-		/// </summary>
-		public bool ContainsKey(object mock)
-		{
-			return inner.Contains(mock);
-		}
-
-		
-		/// <summary>
-		/// Keys
-		/// </summary>
-		public ICollection Keys
-		{
-			get
-			{
-				return inner.Keys;
-			}
-		}
-
-		/// <summary>
-		/// Indexer
-		/// </summary>
-		public IMockState this[object mock]
-		{
-			get
-			{
-				return (IMockState)inner[mock];
-			}
-			set
-			{
-				inner[mock] = value;
-			}
-		}
-
-		/// <summary>
-		/// Number of items
-		/// </summary>
-		public int Count
-		{
-			get { return Keys.Count; }
 		}
 	}
-#endif
 }
