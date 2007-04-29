7faa4d9df5726fff133d3b2ba9d3481955f559a1
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-04-29 00:01:08 +0000

    Castle Bin Update. Fixing an issue where internal interfaces could not be mocked in strongly named assemblies.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@562 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Mocks.Tests/TestInfo.cs
+++ Rhino.Mocks.Tests/TestInfo.cs
@@ -3,5 +3,5 @@ using Rhino.Mocks;
 
 #if dotNet2
 [assembly: InternalsVisibleTo(RhinoMocks.StrongName)]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+//[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 #endif
--- Rhino.Mocks/RhinoMocks.cs
+++ Rhino.Mocks/RhinoMocks.cs
@@ -1,25 +1,24 @@
-using System;
-using System.Text;
-
 namespace Rhino.Mocks
 {
-    /* class: RhinoMocks
+	/* class: RhinoMocks
      * Used for [assembly: InternalsVisibleTo(RhinoMocks.StrongName)]
      */
-    /// <summary>
-    /// Used for [assembly: InternalsVisibleTo(RhinoMocks.StrongName)]
-    /// </summary>
-    public 
-#if dotNet2
-    static 
-#else
-	sealed
-#endif
-	class RhinoMocks
-    {
-        /// <summary>
-        /// Strong name for the Dynamic Proxy assemblies. Used for InternalsVisibleTo specification.
-        /// </summary>
-		public const string StrongName = "DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7";
-    }
+
+	/// <summary>
+	/// Used for [assembly: InternalsVisibleTo(RhinoMocks.StrongName)]
+	/// Used for [assembly: InternalsVisibleTo(RhinoMocks.NormalName)]
+	/// </summary>
+	public static class RhinoMocks
+	{
+		/// <summary>
+		/// Strong name for the Dynamic Proxy assemblies. Used for InternalsVisibleTo specification.
+		/// </summary>
+		public const string StrongName =
+			"DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7";
+
+		/// <summary>
+		/// Normal name for dynamic proxy assemblies. Used for InternalsVisibleTo specification.
+		/// </summary>
+		public const string NormalName = "DynamicProxyGenAssembly2";
+	}
 }
