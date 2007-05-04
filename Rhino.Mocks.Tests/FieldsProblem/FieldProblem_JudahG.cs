59c7b8d445829e50f3b3aa6b1edffdb2c1be3ece
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-05-04 20:43:32 +0000

    Add (passing) test case.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@578 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_JudahG.cs
@@ -0,0 +1,35 @@
+using System;
+using MbUnit.Framework;
+using Rhino.Mocks.Constraints;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+	[TestFixture]
+	public class FieldProblem_JudahG
+	{
+		public interface IView
+		{
+			int? Foo { get; set; }
+		}
+
+		[Test]
+		public void IsMatching()
+		{
+			MockRepository mocks = new MockRepository();
+			IView view = mocks.CreateMock<IView>();
+			using (mocks.Record())
+			{
+				view.Foo = null;
+				Predicate<int> alwaysReturnsTrue = delegate(int input)
+				{
+					return true;
+				};
+				LastCall.Constraints(Is.Matching(alwaysReturnsTrue));
+			}
+			using (mocks.Playback())
+			{
+				view.Foo = 1;
+			}
+		}
+	}
+}
