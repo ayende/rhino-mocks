eae3dd25d2b72a59cc95a72ff93e47bf2e83c313
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-08-18 12:09:51 +0000

    Fixing problem when mocking ADODB.Recordset
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@787 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_dyowee.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ADODB;
+using MbUnit.Framework;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+	[TestFixture]
+	public class FieldProblem_dyowee
+	{
+		[Test]
+		public void MockingRecordSet()
+		{
+			MockRepository mr = new MockRepository();
+			Recordset mock = mr.CreateMock<ADODB.Recordset>();
+			Assert.IsNotNull(mock);
+			Expect.Call(mock.ActiveConnection).Return("test");
+			mr.ReplayAll();
+			Assert.AreEqual("test", mock.ActiveConnection);
+			mr.VerifyAll();
+		}
+	}
+}
