56a4895695117c22b8689144a62ee56ef0d74fd7
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-02-18 06:10:19 +0000

    Adding passing test case.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1209 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Stephan.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using MbUnit.Framework;
+using Rhino.Mocks;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+	using Exceptions;
+
+	[TestFixture]
+	public class FieldProblem_Stefan
+	{
+		// This test fixture relates to ploblems when ignoring arguments on generic method calls when the type is a struct (aka value type).
+		// With reference types - such as String - there is no problem.
+		// It has nothing to do with ordering or not -> but if you do not use an ordered mock recorder, then the error msg is not helpful.
+
+
+		[Test]
+		public void ShouldIgnoreArgumentsOnGenericCallWhenTypeIsStruct()
+		{
+			// setup
+			MockRepository mocks = new MockRepository();
+			ISomeService m_SomeServiceMock = mocks.CreateMock<ISomeService>();
+			SomeClient sut = new SomeClient(m_SomeServiceMock);
+
+			using (mocks.Ordered())
+			{
+				Expect.Call(delegate
+				{
+					m_SomeServiceMock.DoSomething<string>(null, null);
+				});
+				LastCall.IgnoreArguments();
+
+				Expect.Call(delegate
+				{
+					m_SomeServiceMock.DoSomething<DateTime>(null, default(DateTime));  // can't use null here, because it's a value type!
+				});
+				LastCall.IgnoreArguments();
+
+			}
+			mocks.ReplayAll();
+
+			// test
+			sut.DoSomething();
+
+			// verification
+			mocks.VerifyAll();
+
+			// cleanup
+			m_SomeServiceMock = null;
+			sut = null;
+		}
+
+		[Test]
+		[ExpectedException(typeof(ExpectationViolationException), @"ISomeService.DoSomething<System.Int32>(null, 5); Expected #0, Actual #1.
+ISomeService.DoSomething<System.String>(null, ""foo""); Expected #1, Actual #0.")]
+		public void UnexpectedCallToGenericMethod()
+		{
+			MockRepository mocks = new MockRepository();
+			ISomeService m_SomeServiceMock = mocks.CreateMock<ISomeService>();
+			m_SomeServiceMock.DoSomething<string>(null, "foo");
+			mocks.ReplayAll();
+			m_SomeServiceMock.DoSomething<int>(null, 5);
+			mocks.VerifyAll();
+		}
+
+	}
+
+
+	public interface ISomeService
+	{
+		void DoSomething<T>(string key, T someObj);
+	}
+
+
+	internal class SomeClient
+	{
+		private readonly ISomeService m_SomeSvc;
+
+		public SomeClient(ISomeService someSvc)
+		{
+			m_SomeSvc = someSvc;
+		}
+
+		public void DoSomething()
+		{
+			m_SomeSvc.DoSomething<string>("string.test", "some string");
+
+			m_SomeSvc.DoSomething<DateTime>("struct.test", DateTime.Now);
+
+		}
+	}
+}
