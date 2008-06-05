e38763a5615e487e6f4ddfd2348cf8b7611fcd74
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-06-05 00:37:05 +0000

    Adding new CPP project
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1437 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Mocks.CPP.Interfaces/Rhino.Mocks.CPP.Interfaces/Rhino.Mocks.CPP.Interfaces.h
@@ -0,0 +1,13 @@
+// Rhino.Mocks.CPP.Interfaces.h
+
+#pragma once
+
+using namespace System;
+
+namespace RhinoMocksCPPInterfaces {
+
+	public interface class IHaveMethodWithModOpts
+	{
+		void StartLiveOnSlot(long slotNumber);
+	};
+}
