5f8b8b0e9fc739463cd74b402b851c7d8bb3409f
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-10-07 14:27:13 +0000

    Applied (modified) patch from Tim Barcz, sorting out the build process.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1672 079b0acf-d9fa-0310-9935-e5ade295c882

--- release.cmd
+++ release.cmd
@@ -5,5 +5,5 @@ nant zip -D:version=v3.5-for-2.0 -D:build.defines=FOR_NET_2_0
 del ..\build\net-3.5\debug\Rhino.Mocks.???
 nant zip -D:skip.merge.asm=true -D:version=v3.5-unmerged
 del ..\build\net-3.5\debug\Rhino.Mocks.???
-nant zip -D:skip.merge.asm=true -D:version=v3.5-for-2.0-unmerged -D:build.defines=FOR_NET_2_0
+..\SharedLibs\Tools\Nant\nant zip -D:skip.merge.asm=true -D:version=v3.5-for-2.0-unmerged -D:build.defines=FOR_NET_2_0
 
