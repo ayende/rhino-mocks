e7641f248c46d1fd941fe9f8c6acc93685d5791c
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-11-14 14:58:33 +0000

    Added passing test.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1726 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Adam.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel;
+using MbUnit.Framework;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+    [TestFixture]
+    public class FieldProblem_Adam
+    {
+        public interface IFoo
+        {
+            string Str { get; set; }
+            event EventHandler Event;
+        }
+
+
+        [Test]
+        public void ShouldRaiseEventWhenEverPropIsSet()
+        {
+            var foo = MockRepository.GenerateMock<IFoo>();
+            foo.Stub(x => x.Str = Arg<string>.Is.Anything)
+                .WhenCalled(x => foo.Raise(y => y.Event += null, foo, EventArgs.Empty));
+
+            int calls = 0;
+            foo.Event += delegate
+            {
+                calls += 1;
+            };
+
+            foo.Str = "1";
+            foo.Str = "2";
+            foo.Str = "3";
+            foo.Str = "4";
+
+            Assert.AreEqual(4, calls);
+        }
+    }
+}
