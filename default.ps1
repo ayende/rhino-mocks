666707bc8a3ebc7a3aeaff2b5e86f35a6a21a8f3
Ayende Rahien <ayende@ayende.com>
2009-09-10 07:38:28 +0300

    Better build name

--- default.ps1
+++ default.ps1
@@ -93,7 +93,7 @@ task Merge {
 
 task Release -depends Test, Merge {
 	& $tools_dir\zip.exe -9 -A -j `
-		$release_dir\Rhino.Mocks-$humanReadableversion-$env:ccnetnumericlabel.zip `
+		$release_dir\Rhino.Mocks-$humanReadableversion-Build-$env:ccnetnumericlabel.zip `
 		$build_dir\Rhino.Mocks.dll `
 		$build_dir\Rhino.Mocks.xml `
 		license.txt `
@@ -106,7 +106,7 @@ task Release -depends Test, Merge {
 task Upload -depend Release {
 	if (Test-Path $uploadScript ) {
 		$log = git log -n 1 --oneline		
-		msbuild $uploadScript /p:Category=$uploadCategory "/p:Comment=$log" "/p:File=$release_dir\Rhino.Mocks-$humanReadableversion-$env:ccnetnumericlabel.zip"
+		msbuild $uploadScript /p:Category=$uploadCategory "/p:Comment=$log" "/p:File=$release_dir\Rhino.Mocks-$humanReadableversion-Build-$env:ccnetnumericlabel.zip"
 		
 		if ($lastExitCode -ne 0) {
 			throw "Error: Failed to publish build"
