d6af93e757cd629b2b85f8e2b96f304036347898
Chris Missal <chris.missal@gmail.com>
2009-11-28 16:58:04 -0600

    Added blank AssemblyInfo.cs to the GettingStarted project and one unit test

--- Rhino.Mocks.GettingStarted/Stubs.cs
+++ Rhino.Mocks.GettingStarted/Stubs.cs
@@ -18,12 +18,42 @@ namespace Rhino.Mocks.GettingStarted
         }
 
         /// <summary>
+        /// When you need to mock a read-only property of a class.
+        /// </summary>
+        [Test]
+        public void How_to_Stub_out_your_own_value_of_a_ReadOnlyProperty()
+        {
+            // Arrange
+            var foo = MockRepository.GenerateStub<IFoo>();
+            foo.Stub(x => x.ID).Return(123);
+
+            // Act
+            var id = foo.ID;
+
+            // Assert
+            Assert.That(id, Is.EqualTo(123));
+        }
+
+        /// <summary>
         /// This is just a sample interface, what it is or does isn't really relevant. It could
         /// be IUser of IOrder
         /// </summary>
         public interface IFoo
         {
+            int ID { get; }
             string Name { get; set; }
+        }
+
+        public class Foo : IFoo
+        {
+            private int id;
+
+            public int ID
+            {
+                get { return id; }
+            }
+
+            public string Name { get; set; }
         }
     }
 }
